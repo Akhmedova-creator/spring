bc3ef7c4d6116d7c427d264654e1e0a8af2d5ce3
Akhmedova-creator <55805446+Akhmedova-creator@users.noreply.github.com>
2021-07-11 14:34:56 +0500

    Delete spring_data directory

--- spring_data/src/main/java/ru/otus/spring/repository/AuthorsRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.otus.spring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.otus.spring.domain.Authors;
-
-public interface AuthorsRepository extends JpaRepository<Authors, Long> {
-    Authors findByFirstName(String name);
-}
--- spring_data/src/main/java/ru/otus/spring/service/ServiceAuthors.java
+++
@@ -1,9 +0,0 @@
-package ru.otus.spring.service;
-
-import ru.otus.spring.domain.Authors;
-
-import org.springframework.data.domain.Sort;
-public interface ServiceAuthors {
-    Iterable<Authors> getAuthors(Sort sort);
-    Authors findByFirstName(String name);
-}
