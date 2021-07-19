b54195ccc56d96728c7929b535c3b50a941e962f
Akhmedova-creator <55805446+Akhmedova-creator@users.noreply.github.com>
2021-07-20 00:09:24 +0500

    Delete spring__MVC directory

--- spring__MVC/src/main/java/ru/otus/spring/service/ServiceComment.java
+++
@@ -1,9 +0,0 @@
-package ru.otus.spring.service;
-
-import ru.otus.spring.doman.Comment;
-
-import java.time.LocalDate;
-
-public interface ServiceComment {
-    Comment getCommentByDate(LocalDate date);
-}
