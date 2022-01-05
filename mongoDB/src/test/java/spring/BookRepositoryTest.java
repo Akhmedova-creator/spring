26c3261aca09a3d24d6d9a79bcf1fe29c5f99fd9
sdddd <fazik_agent007@mail.ru>
2022-01-05 10:01:59 +0500

    refactor name modules

--- mongoDB/src/test/java/spring/BookRepositoryTest.java
+++ mongoDB/src/test/java/spring/BookRepositoryTest.java
@@ -1,16 +1,9 @@
 package spring;
-import com.github.cloudyrock.spring.v5.EnableMongock;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import spring.doman.Authors;
 import spring.doman.Books;
 import spring.doman.Comments;
